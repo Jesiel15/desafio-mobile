64bb1161e65fb8208b3bec45904bfd72246a0907
˜Jesiel <fariajesiel@gmail.com>
2021-03-04 09:17:49 -0300

    Retirando TabnNavigator e acrescentando Stack

--- App.js
+++ App.js
@@ -2,32 +2,18 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Home from './src/pages/Home';
 import Detail from './src/pages/Detail';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Tab = createBottomTabNavigator();
+import { createStackNavigator } from '@react-navigation/stack';
 
-function MyTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen options={{
-        tabBarVisible: false
-      }}
-        name="Home"
-        component={Home} />
-      <Tab.Screen
-       options={{
-        tabBarVisible: false
-      }}
-        name="Detail"
-        component={Detail} />
-    </Tab.Navigator>
-  );
-}
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer >
-      <MyTabs />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Detail" component={Detail} />
+      </Stack.Navigator>
     </NavigationContainer >
   )
 }
